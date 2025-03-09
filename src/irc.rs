866fa1508b4629cdd1d747d5be4950f19fe60055
v.drachenko <only-victor@mail.ru>
2025-03-09 12:16:16 +0300

    Правка

--- src/irc.rs
+++ src/irc.rs
@@ -3,3 +3,11 @@
 #![allow(non_snake_case)]
 
 include!(concat!(env!("OUT_DIR"), "/irc_bindings.rs"));
+
+// Manual bind C function
+//
+// #[link(name = "ircclient")]
+// unsafe extern "C" {
+//     pub fn irc_create_session(callbacks: *const libc::c_void)
+//              -> *const libc::c_void;
+// }
--- src/prelude.rs
+++ src/prelude.rs
@@ -5,14 +5,6 @@ extern crate alloc;
 use libc_print::std_name::*;
 use {alloc::boxed::Box, core::ffi::c_char, libc::sprintf};
 
-// Manual bind C function
-//
-// #[link(name = "ircclient")]
-// unsafe extern "C" {
-//     pub fn irc_create_session(callbacks: *const libc::c_void)
-//              -> *const libc::c_void;
-// }
-
 #[no_mangle]
 pub extern "C" fn hello_lib() -> *const c_char {
     let buffer: *mut c_char = Box::into_raw(Box::new([0u8; 100])) as *mut i8;
